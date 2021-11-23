b05edb3e44481766e5264582f1be3d879b35d2e9
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-11-23 13:00:01 -0800

    update

--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -7,11 +10,29 @@ from .forms import RoomForm
 #     {'id':2, 'name':'Design with me'},
 #     {'id':3, 'name':'Frontend Developers'},
 # ]
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'User does not exist')
+    context = {}
+    return render(request,'base/login_register.html',context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+    Q(topic__name__icontains=q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
-    context = {'rooms': rooms,'topics':topics}
+    room_count = rooms.count()
+
+    context = {'rooms': rooms,'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):
