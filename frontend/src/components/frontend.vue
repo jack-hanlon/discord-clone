41644a1829ed2d8c7af157a9d6dbd6669a68a9ca
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2021-11-29 15:50:21 -0800

    update to frontend

---
+++ frontend/src/components/frontend.vue
@@ -0,0 +1,34 @@
+<template>
+
+
+  <div id="rooms-container">
+    <h1>Cool Rooms</h1>
+    {{rooms}}
+  </div>
+
+
+</template>
+
+
+<script>
+
+export default {
+  name: "frontend",
+  data: () => ({
+    rooms: getRooms()
+  }),
+  props: {
+
+  },
+  methods: {
+    async getRooms() {
+      let response = await fetch('http://127.0.0.1:8000/api/rooms/')
+      let rooms = await response.json()
+      return rooms
+    }
+
+
+  }
+
+
+</script>
